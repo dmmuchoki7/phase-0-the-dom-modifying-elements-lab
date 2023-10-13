72b679fc8ed793aa031e782adffdf13a0627d491
dmmuchoki7 <dmmuchoki7@gmail.com>
2023-10-13 12:32:02 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+//Remove main tag
+const main= document.querySelector("#main").remove();
+
+//create H1
+    const newHeader = document.createElement("h1");
+    newHeader.id= "victory";
+    newHeader.textContent = "David is the champion";
